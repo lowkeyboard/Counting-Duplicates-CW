af382f8af71d282b336c012de153306f8a7a53b6
caglacprgl@outlook.com <caglacprgl@outlook.com>
2020-07-06 20:24:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace CountingDuplicates
+{
+    class Program
+    {
+
+        public static int DuplicateCount(string str)
+        {
+            
+            str.ToUpper();
+            char[] charArr = str.ToCharArray();
+
+            Array.Sort(charArr);
+
+
+            string newStr = new string(charArr);
+            newStr = newStr.ToLower();
+
+
+            char[] charX = newStr.ToCharArray();
+
+            Array.Sort(charX);
+
+
+            string SnewStr = new string(charX);
+
+            // this did not work out for some reason string pattern = @"[0-9a-z]+";    //@"(\w)\1";
+            // so here is another way: 
+
+            return SnewStr
+                .GroupBy(x => x)
+                .Count(x => x.Count() > 1);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DuplicateCount("Indivisibilities")); 
+
+        }
+    }
+}
